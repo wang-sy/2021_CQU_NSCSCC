efd6bdffc7344b7d5f1c161403512506361b518a
FanQinFred <1836459118@qq.com>
2020-12-30 15:34:37 +0800

    MEM阶段的异常处理完毕

--- MEM/MEM.sv
+++ MEM/MEM.sv
@@ -10,6 +10,12 @@ module MEM (
     input logic                 mt_hi_i,
     input logic                 mt_lo_i,
 
+    input logic                 rmem_i,
+    input logic                 wmem_i,
+    input logic [`AluOpBus]     aluop_i,
+    input logic [`DataBus]      ram_data_i,
+    input logic [31:0]          mem_io_addr_i,
+
     input logic [`RegAddrBus]   exception_type_i,
     input logic [`RegAddrBus]   current_instr_addr_i,
     input logic                 is_in_delayslot_i,
@@ -22,18 +28,52 @@ module MEM (
 	input logic [4:0]           wb_cp0_reg_write_addr,
 	input logic [`RegAddrBus]   wb_cp0_reg_data,
 
+
     output logic[`DataBus]      hi_o,
     output logic[`DataBus]      lo_o,
+    output logic[`DoubleRegBus] wdata_o,
+    output logic[`DataBus]      ram_wdata_o,
+    output logic[31:0]          ram_addr_o,
+    output logic[3:0]           ram_sel_o,
+    output logic                ram_we_o,
+    output logic                ram_ce_o,
 
-    output logic[31:0]          excepttion_type_o,
+    output logic[31:0]          exception_type_o,
 	output logic[`RegAddrBus]   current_instr_address_o,
 
 	output logic                is_in_delayslot_o,
 	output logic[`RegAddrBus]   cp0_epc_o
+
 );
 
     assign is_in_delayslot_o = is_in_delayslot_i;
     assign current_instr_address_o = current_instr_address_i;
+
+    logic [`RegAddrBus] CP0_Status_Newest;
+    logic [`RegAddrBus] CP0_Cause_Newest;
+    logic [`RegAddrBus] CP0_Epc_Newest;
+    
+    //最新的Staus寄存器
+    assign CP0_Status_Newest = rst_i == 1'b1 ? 32'b0 ; 
+                               (wb_cp0_reg_we==1'b1 && wb_cp0_reg_write_addr==`CP0_REG_STATUS) ? wb_cp0_reg_data : cp0_status_i;
+    //最新的Cause寄存器
+    assign CP0_Cause_Newest    = rst_i == 1'b1 ? 32'b0 ; 
+                               (wb_cp0_reg_we==1'b1 && wb_cp0_reg_write_addr==`CP0_REG_CAUSE) ? 
+                               {{cp0_cause_i[31:24]},{wb_cp0_reg_data[23:22]},{cp0_cause_i[21:10]},{wb_cp0_reg_data[9:8]},{cp0_cause_i[7:0]}} : cp0_cause_i;
+    //最新的Epc寄存器
+    assign CP0_Epc_Newest      = rst_i == 1'b1 ? 32'b0 ; 
+                               (wb_cp0_reg_we==1'b1 && wb_cp0_reg_write_addr==`CP0_REG_EPC) ? wb_cp0_reg_data : cp0_epc_i;
+    //输出最新cp0_epc
+    assign cp0_epc_o = CP0_Epc_Newest;
+
+    //exception_type_o
+    exception exception_type(
+        .rst_i(rst_i),
+        .exception_type_i(exception_type_i),
+        .cp0_status_i(cp0_status_i),
+        .cp0_cause_i(cp0_cause_i),
+        .exception_type_o(exception_type_o)
+    );
 
     /*assign {wd_o, wreg_o, wdata_o} = (rst_i == 1'b1) ? {`NOPRegAddr, 1'b0, `ZeroWord} :
                                      {wd_i, wreg_i, wdata_i};*/
@@ -47,5 +87,28 @@ module MEM (
         .w_lo_i(mt_lo_i),
         .hi_o(hi_o),
         .lo_o(lo_o)
+    );
+
+    logic [`DataBus]  mem_read_data;
+    assign wdata_o = (aluop_i == `EXE_LB_OP ||  aluop_i == `EXE_LBU_OP ||  aluop_i == `EXE_LH_OP ||
+                      aluop_i == `EXE_LHU_OP ||  aluop_i == `EXE_LW_OP ) ? {mem_read_data, mem_read_data} : wdata_i;
+    // 对io进行控制
+    // 有异常就取消对MEMORY的写操作
+    assign logic wmem_i_last = wmem_i & (~(|exception_type_o));
+
+    mem_io_controller mem_mem_io_controller(
+        .aluop_i(aluop_i),
+        .wdata_i(wdata_i),
+        .mem_io_addr_i(mem_io_addr_i),
+        .rmem_i(rmem_i),
+        .wmem_i(wmem_i_last),
+        .ram_data_i(ram_data_i),
+
+        .ram_wdata_o(ram_wdata_o),
+        .ram_addr_o(ram_addr_o),
+        .ram_sel_o(ram_sel_o),
+        .ram_we_o(ram_we_o),
+        .ram_ce_o(ram_ce_o),
+        .read_data_o(mem_read_data)
     );
 endmodule
--- exception.sv
+++ exception.sv
@@ -1,31 +1,19 @@
 module exception(
-	input  logic       rst,
-	input  logic [7:0] except,
-	input  logic [4:0] tlb_except2M,
-	input  logic       Trap,
-    input  logic       CpU,
-	input  logic       adel,
-    input  logic       ades,
-	input  logic[31:0] cp0_status,
-    input  logic[31:0] cp0_cause,
-	output logic[31:0] excepttype
+	input  logic        rst_i,
+
+	input  logic [31:0] exception_type_i,
+	input  logic [31:0] cp0_status_i,
+    input  logic [31:0] cp0_cause_i,
+
+	output logic [31:0] exception_type_o
 );
 
-assign excepttype = rst==1'b1 ? 32'b0 : 
-                    (((cp0_cause[15:8] & cp0_status[15:8]) != 8'h00) &&(cp0_status[1] == 1'b0) && (cp0_status[0] == 1'b1))==1'b1 ? 32'h00000001 : 
-                    (except[7] == 1'b1 || adel == 1'b1)==1'b1?32'h00000004 :
-                    ades==1'b1?32'h00000005 :
-                    Trap==1'b1?32'h0000000d :
-                    CpU==1'b1?32'h0000000b :
-                    tlb_except2M[4] == 1'b1?32'h00000010 :
-                    tlb_except2M[3] == 1'b1?32'h00000011 :
-                    tlb_except2M[2] == 1'b1?32'h00000012 :
-                    tlb_except2M[1] == 1'b1?32'h00000013 :
-                    tlb_except2M[0] == 1'b1?32'h00000014 :
-                    except[6] == 1'b1?32'h00000008 :
-                    except[5] == 1'b1?32'h00000009 :
-                    except[4] == 1'b1?32'h0000000e :
-                    except[3] == 1'b1?32'h0000000a :
-                    except[2] == 1'b1?32'h0000000c : 32'b0;
+assign exception_type_o =   rst_i == 1'b1 ? 32'b0 : 
+                            (((cp0_cause[15:8] & cp0_status[15:8]) != 8'h00) &&(cp0_status[1] == 1'b0) && (cp0_status[0] == 1'b1))==1'b1 ? 32'h00000001 : 
+                            exception_type_i[8]   ==   1'b1?32'h00000008 :
+                            exception_type_i[9]   ==   1'b1?32'h0000000a :
+                            exception_type_i[11]  ==   1'b1?32'h0000000c :
+                            exception_type_i[10]  ==   1'b1?32'h0000000d :
+                            exception_type_i[12]  ==   1'b1?32'h0000000e :  32'b0;
 endmodule
 
