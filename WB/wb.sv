d07001c21be22fc517a74ebb63b9cd5c907dd5ab
FanQinFred <1836459118@qq.com>
2020-12-30 15:37:22 +0800

    WB异常处理完成

--- WB/wb.sv
+++ WB/wb.sv
@@ -8,6 +8,8 @@ module wb(
     input  logic                    mem_reg,
     input  logic [`DoubleRegBus]    mem_wdata,
 
+    input  logic                    flush_i,
+
     output logic  [4:0]             wb_wd,
     output logic                    wb_reg,
     output logic  [`DataBus]        wb_wdata
@@ -18,7 +20,13 @@ always@(posedge clk) begin
         wb_wd      <=  5'b0;
         wb_reg     <=  1'b0;
         wb_wdata   <=  32'b0;
-    end else begin
+    end
+    else if(flush_i==1'b1) begin
+        wb_wd      <=  5'b0;
+        wb_reg     <=  1'b0;
+        wb_wdata   <=  32'b0;
+    end 
+    else begin
         wb_wd       <=  mem_wd;
         wb_reg      <=  mem_reg;
         wb_wdata    <=  mem_wdata[31:0];
